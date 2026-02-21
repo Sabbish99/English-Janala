784649bf8f54c672f44ffc3068dd38ddd2a109a6
Sabbish99 <programmersabbir99@gmail.com>
2026-02-21 21:39:50 +0600

    card add

--- js/index.js
+++ js/index.js
@@ -13,15 +13,31 @@ const loadLevelWord = (id) => {
 const displayLevelWord = (words) => {
     const wordContainer = document.getElementById('word-container')
     wordContainer.innerHTML = ""
+
+    if (words.length == 0) {
+        wordContainer.innerHTML = `
+        <div class="text-center  col-span-full rounded-xl py-10 space-y-6 font-bangla">
+         <img class="mx-auto" src="./assets/alert-error.png" alt="">
+            <p class="text-xl font-medium text-gray-500">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+        </div>
+        
+        `
+        return
+    }
+
+
+
+
     words.forEach(word => {
         console.log(word);
 
         const card = document.createElement('div')
         card.innerHTML = `
  <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-            <h2 class="font-bold text-2xl">${word.word}</h2>
+            <h2 class="font-bold text-2xl">${word.word ? word : "শব্দের অর্থ পাওয়া যায়নি।"}</h2>
             <p class="font-semibold">sfsdf</p>
-            <div class="text-2xl font-medium font-bangla">${word.meaning} / ${word.pronunciation}</div>
+            <div class="text-2xl font-medium font-bangla">${word.meaning ? word.meaning : "শব্দের অর্থ পাওয়া যায়নি।"} / ${word.pronunciation ? word.pronunciation : "শব্দের অর্থ পাওয়া যায়নি।"}</div>
             <div class="flex justify-between items-center">
                 <button class="btn bg-[#1A91FF20] hover:bg-[#1A91FF60]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF20]  hover:bg-[#1A91FF60]"><i class="fa-solid fa-volume-low"></i></button>
